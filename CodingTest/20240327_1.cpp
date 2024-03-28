94a55ad4f07c5c13284b9abbaab0644c1744d92c
Lee Yoon Seok <kjggr123@gmail.com>
2024-03-28 22:10:44 +0900

    타겟 넘버
    https://lead-guitar-e0c.notion.site/078ace3741934ed6ba38f00b9c01208e?pvs=4

--- CodingTest/20240327_1.cpp
+++ CodingTest/20240327_1.cpp
@@ -15,30 +15,25 @@ vector<int> solution(vector<int> sequence, int k) {
     auto endP = sequence.rbegin();
 
     vector<int> answer;
+    answer.push_back(-1);
+    answer.push_back(-1);
 
-
-    for(auto p = sequence.rbegin(); p!=sequence.rend(); ++p)
+    for (auto p = sequence.rbegin(); p != sequence.rend(); ++p)
     {
-        if(!answer.empty() && distance(startP,endP) > (answer[1]-answer[0]+1))
-        {
-            return answer;
-        }
-
-
-
         // == k 보다 큰 숫자는 볼 필요 없음.
         if (*p > k) {
             CurrentSum = 0;
             continue;
         }
 
-
-
-
+        if (answer[0]!=-1 && (distance(startP, endP) > (answer[1] - answer[0] + 1)))
+        {
+            return answer;
+        }
 
         // == 합해서 k가 되도록 계속 찾기
         // 첫 범위 라면, 스타트 위치를 정해주고
-        if(CurrentSum == 0)
+        if (CurrentSum == 0)
         {
             startP = p;
         }
@@ -46,38 +41,30 @@ vector<int> solution(vector<int> sequence, int k) {
         // 더해지는 곳 까지 endp 를 저장해준다.
         endP = p;
 
-
-
-
-
-
         // 만약 더하던 값이 k를 넘어가면 초기화 후 이전 시작위치 다음부터 시작한다.
-        if(CurrentSum>k)
+        if (CurrentSum > k)
         {
             CurrentSum = 0;
             p = startP;
-            ++p;
-            startP = endP = sequence.rbegin();
             continue;
         }
         // == 만약 k 값이 됐다면 값 출력 하지만, begin 기준으로 출력해야함
-    	else if(CurrentSum == k)
+        else if (CurrentSum == k)
         {
             // == (추가작업) 다만, 범위가 같다는 한에서 앞쪽을 해야 정답이므로,
-			// == 이후 작업도 진행해주어야한다.
-            if(answer.size()!=0)
+            // == 이후 작업도 진행해주어야한다.
+            if (answer[0]!=-1)
             {
                 int firstDistance = answer[1] - answer[0] + 1;
                 int fwd = sequence.size() - distance(sequence.rbegin(), endP) - 1;
                 int bwd = sequence.size() - distance(sequence.rbegin(), startP) - 1;
                 int NowDistance = bwd - fwd + 1;
 
-                if(firstDistance >= NowDistance)
+                if (firstDistance >= NowDistance)
                 {
-                    answer.clear();
-                    answer.push_back(fwd);
-                    answer.push_back(bwd);
-                    p = startP;
+                    answer[0] = fwd;
+                    answer[1] = bwd;
+                    p = endP;
                     continue;
                 }
                 else
@@ -85,13 +72,14 @@ vector<int> solution(vector<int> sequence, int k) {
                     return answer;
                 }
             }
-    		else
+            else
             {
                 // startP가 begin 기준 뒷쪽
                 // endp가 begin 기준 앞쪽
                 // rbegin 이므로 -1을 해주어서 값을 통일시켜준다.
-                answer.push_back(sequence.size() - distance(sequence.rbegin(), endP) - 1);
-                answer.push_back(sequence.size() - distance(sequence.rbegin(), startP) - 1);
+                answer[0] = (sequence.size() - distance(sequence.rbegin(), endP) - 1);
+                answer[1] = (sequence.size() - distance(sequence.rbegin(), startP) - 1);
+
             }
         }
     }
---
+++ CodingTest/20240328.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 모든 숫자 입력에 대해서 +인 경우와 - 인경우 두가지로 갈래가 나눠지는 이진 트리 구조로 진행을 하여
+// 맨 아래 노드의 값이 타겟인 경우의 수를 더하면 될 것 같다.
+
+int Value = 0;
+
+void CalcValue(std::vector<int>& numbers, int currentIndex, int NowValue, int TargetValue)
+{
+    if (currentIndex >= numbers.size()) {
+        if (NowValue == TargetValue)
+        {
+            Value += 1;
+        }
+        return;
+    }
+
+    CalcValue(numbers, currentIndex + 1, NowValue + numbers[currentIndex], TargetValue);
+    CalcValue(numbers, currentIndex + 1, NowValue - numbers[currentIndex], TargetValue);
+}
+
+int solution(vector<int> numbers, int target) {
+    
+
+    // 트리에 numbers 구조를 다 더할 순 없으니,
+    // 순환 구조 개념으로 접근을 해본다.
+    CalcValue(numbers, 0, 0, target);
+
+    int answer = Value;
+    std::cout << answer << std::endl;
+    return answer;
+}
+
+void main()
+{
+    solution(std::vector<int>{1, 1, 1, 1, 1}, 3);
+}
