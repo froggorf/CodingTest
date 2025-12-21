e17b534e3846051facb775b87fdd2fd785c90f42
froggorf <113008438+froggorf@users.noreply.github.com>
2025-12-21 21:47:05 +0900

    [Gold I] Title: 구간 합 구하기, Time: 204 ms, Memory: 41088 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+std::vector<long long> Nums;
+std::vector<long long> SegmentTree;
+
+long long Init(int Node, int Start, int End) {
+	if (Start == End) {
+		return SegmentTree[Node] = Nums[Start];
+	}
+
+	int Mid = (Start + End) / 2;
+	return SegmentTree[Node] = Init(Node * 2 + 1, Start, Mid) + Init(Node*2+2, Mid+1,End);
+}
+
+void Update(int Node, int Start, int End, int Idx, long long NewValue) {
+	if (Idx < Start || End < Idx) {
+		return;
+	}
+
+	if (Start == End) {
+		SegmentTree[Node] = NewValue;
+		return;
+	}
+
+	int Mid = (Start + End) / 2;
+	Update(Node * 2 + 1, Start, Mid, Idx, NewValue);
+	Update(Node * 2 + 2, Mid + 1, End, Idx, NewValue);
+	SegmentTree[Node] = SegmentTree[Node * 2 + 1] + SegmentTree[Node * 2 + 2];
+}
+
+long long Query(int Node, int Start, int End, int Left, int Right) {
+	if (Right < Start || End < Left) {
+		return 0;
+	}
+
+	if (Left <= Start && End <= Right) {
+		return SegmentTree[Node];
+	}
+
+	int Mid = (Start + End) / 2;
+	return Query(Node * 2 + 1, Start, Mid, Left, Right) + Query(Node * 2 + 2, Mid + 1, End, Left, Right);
+}
+
+int main() {
+	// 구간합은 DP 나 세그먼트 트리
+	// 자주 바뀌면 세그먼트 트리
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	long long N, M, K;
+	std::cin >> N >> M >> K;
+	Nums = std::vector<long long>(N);
+	for (int i = 0; i < N; ++i) {
+		std::cin >> Nums[i];
+	}
+	SegmentTree = std::vector<long long>(N * 4);
+	Init(0, 0, N - 1);
+	for (int i = 0; i < M + K; ++i) {
+		long long Command, A, B;
+
+		std::cin >> Command >> A >> B;
+		--A;
+		if (Command == 1) {
+			Update(0, 0, N - 1, A, B);
+		}
+		else {
+			std::cout << Query(0, 0, N - 1, A, B - 1) << "\n";
+		}
+	}
+}
