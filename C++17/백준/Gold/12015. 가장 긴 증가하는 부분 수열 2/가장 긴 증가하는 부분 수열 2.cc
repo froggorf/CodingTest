0d210f0d699a791455a8a0d0d2e65244ed08b4aa
froggorf <113008438+froggorf@users.noreply.github.com>
2026-04-03 13:20:32 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 136 ms, Memory: 12200 KB -BaekjoonHub

--- C++17/백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ C++17/백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,33 +1,39 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
-	// nlogn 으로 끊어야함
+int main() {
+	// n이 100만이기 때문에 nlogn으로 끊어야함
+	// 그렇다면...
 	std::cin.tie(nullptr);
+	std::ios::sync_with_stdio(false);
+
 	int N;
-	std::cin>>N;
+	std::cin >> N;
 	std::vector<int> Nums(N);
-	for (int i = 0; i <N; ++i)
-	{
+	for (int i = 0; i < N; ++i) {
 		std::cin>>Nums[i];
 	}
 
 	std::vector<int> Answers;
-	Answers.reserve(N);
-	for (int Num : Nums)
-	{
-		auto Lower = std::lower_bound(Answers.begin(), Answers.end(), Num);
-		if (Lower == Answers.end())
-		{
-			Answers.emplace_back(Num);
+
+	for (int i = 0; i < N; ++i) {
+		// 일단 하나씩 돌면서
+
+		// 맨 뒤의 수보다 작은 수라면 적절한 위치에 대체해줘야함
+		
+		if (!Answers.empty() && Answers.back() >= Nums[i]) {
+			if (Answers.back() == Nums[i]) {
+				continue;
+			}
+			auto Iter = std::lower_bound(Answers.begin(), Answers.end(), Nums[i]);
+			*Iter = Nums[i];
+			continue;
 		}
-		else
-		{
-			*Lower = Num;
-		}
+
+		// 아니면 걍 추가하면 됨
+		Answers.emplace_back(Nums[i]);
 	}
 
-	std::cout<<Answers.size();
-	
+	std::cout << Answers.size();
+
 }
