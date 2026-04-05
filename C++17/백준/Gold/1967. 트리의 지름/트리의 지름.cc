5787b6d6a79ee28506e4392c47ebccb7fffcc074
froggorf <113008438+froggorf@users.noreply.github.com>
2026-04-05 22:02:49 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 2788 KB -BaekjoonHub

--- C++17/백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ C++17/백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,86 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
 int N;
-std::vector<std::vector<std::pair<int, int>>> Graph;
-int FindFarNode(int StartNode) {
-	std::queue<std::pair<int, int>> Q;
-	Q.emplace(StartNode, 0);
-	int FarNode = StartNode;
-	int FarDistance = 0;
+std::pair<int,int> FindBestFarNode(
+	const std::vector<std::vector<std::pair<int, int>>>& Graph,
+	int StartNode) {
+	std::pair<int, int> ReturnValue{ StartNode,0 };
+	
+	std::stack<std::pair<int, int>> S;
 	std::vector<bool> Visited(N + 1, false);
-	while (!Q.empty()) {
-		std::pair<int, int> Cur = Q.front(); Q.pop();
-		if (Visited[Cur.first]) {
-			continue;
-		}
-		Visited[Cur.first] = true;
-		if (FarDistance < Cur.second) {
-			FarDistance = Cur.second;
-			FarNode = Cur.first;
-		}
-
-		for (const std::pair<int, int>& NodeData : Graph[Cur.first]) {
-			if (Visited[NodeData.first]) {
+	S.emplace(StartNode, 0);
+	Visited[StartNode] = true;
+	while (!S.empty()) {
+		std::pair<int, int> Top = S.top(); S.pop();
+		for (const std::pair<int, int>& NextNodes : Graph[Top.first]) {
+			// 이미 방문했던 곳이면 패스
+			if (Visited[NextNodes.first]) {
 				continue;
 			}
-			Q.emplace(NodeData.first, Cur.second + NodeData.second);
+			// 아니면 추가한다
+			S.emplace(NextNodes.first, Top.second + NextNodes.second);
+			Visited[NextNodes.first] = true;
+		}
+		if (ReturnValue.second < Top.second) {
+			ReturnValue = Top;
 		}
 	}
 
-	return FarNode;
+	return ReturnValue;
 }
+
 int main() {
-	// 가중치가 1이 아닌 그래프에서 최장길이를 구하는거니까
-	// 플루이드 워셜을 쓰면 됨
-	// 내가 기억하기로 이건 DP를 활용하는것이었는데..
-	
-	// 아니다 최단경로를 구해서 그 최단경로가 가장 긴것을 구하는것 같음
+	// 이 문제 한 노드 잡고 제일 먼거 구한다음에
+	// 걔로부터 가장 먼걸 구해서 그 둘사이의 길이를 구하면 됐던거같음
+	// 왜냐면 첫번쨰 가장먼게 가장 첫 먼 지름한쪽이고
+	// 그다음 먼건 반대쪽 지름이니까
 
 	std::cin.tie(nullptr);
+	std::ios::sync_with_stdio(false);
 	std::cin >> N;
-	Graph = std::vector<std::vector<std::pair<int, int>>>(N + 1);
+	std::vector<std::vector<std::pair<int, int>>> Graph(N+1);
 	for (int i = 0; i < N-1; ++i) {
-		int Start;
-		int End;
-		int Weight;
-		std::cin >> Start >> End >> Weight;
-		Graph[Start].emplace_back(End, Weight);
-		Graph[End].emplace_back(Start, Weight);
+		int Start, End, Value;
+		std::cin >> Start >> End >> Value;
+		Graph[Start].emplace_back(End, Value);
+		Graph[End].emplace_back(Start, Value);
 	}
 
-	int Answer = 0;
-	// 노드의 개수가 10000개,
-	// 100'000'000 1억.. 시간제한은 2초,,
-	// nlogn으로 끊는게 맞음,
+	// 일단 점 하나 잡고, 그곳에서 가장 먼걸 구한다
+	std::pair<int, int> FarFromFirstNode = FindBestFarNode(Graph, 1);
+	std::pair<int, int> Answer = FindBestFarNode(Graph, FarFromFirstNode.first);
 
-	// 노드 하나 잡아서
-	int FirstFarNode = FindFarNode(1);
-	int AnotherFarNode = FindFarNode(FirstFarNode);
-	
-	std::queue<std::pair<int, int>> Q;
-	Q.emplace(FirstFarNode, 0);
-	std::vector<bool> Visited(N + 1, false);
-	while (!Q.empty()) {
-		std::pair<int, int> Cur = Q.front(); Q.pop();
-		
-		if (Cur.first == AnotherFarNode) {
-			std::cout << Cur.second;
-			return 0;
-		}
-
-		if (Visited[Cur.first]) {
-			continue;
-		}
-		Visited[Cur.first] = true;
+	std::cout << Answer.second;
 
 
-		for (const std::pair<int, int>& NextNode : Graph[Cur.first]) {
-			if (Visited[NextNode.first]) {
-				continue;
-			}
-			Q.emplace(NextNode.first, Cur.second + NextNode.second);
-		}
-	}
-	
+
+
 }
