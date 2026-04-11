511d652883b121f5f968b7f003ea5dab003caca9
froggorf <113008438+froggorf@users.noreply.github.com>
2026-04-11 13:35:14 +0900

    [Gold V] Title: 최소비용 구하기, Time: 24 ms, Memory: 4748 KB -BaekjoonHub

--- C++17/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ C++17/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,62 +1,49 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 
-int main()
-{
+int main() {
 	std::cin.tie(nullptr);
 	std::ios::sync_with_stdio(false);
 
 	int N, M;
-	std::cin>>N>>M;
+	std::cin >> N >> M;
+	struct Bus {
+		int End, Value;
+	};
+	std::vector<std::vector<Bus>> BusData(N+1);
 
-	std::vector<std::vector<std::pair<int,int>>> Costs(N+1);
-	for (int i = 0; i < M; ++i)
-	{
-		int Start, End, Cost;
-		std::cin >> Start >> End >> Cost;
-		Costs[Start].emplace_back(End,Cost);
+	for (int i = 0; i < M; ++i) {
+		int Start, End, Value;
+		std::cin >> Start >> End >> Value;
+		BusData[Start].emplace_back(Bus{ End, Value });
 	}
+	int StartCity, EndCity;
+	std::cin >> StartCity >> EndCity;
+	std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> PQ;
+	PQ.emplace(0, StartCity);
+	std::vector<int> Dist(N + 1, INT32_MAX);
+	while (!PQ.empty()) {
+		int CurValue = PQ.top().first;
+		int CurCity = PQ.top().second; 
+		PQ.pop();
 
-	int A, B;
-	std::cin>>A>>B;
-	
-	std::vector<int> Dist(N+1, 0x3f3f3f3f);
-	// A에서 출발하니까 Dist[A] = 0;
-	Dist[A] = 0;
+		if (CurValue > Dist[CurCity]) {
+			continue;
+		}
 
-	std::priority_queue<std::pair<int, int>, 
-		std::vector<std::pair<int, int>>, 
-		std::greater<std::pair<int, int>>> PQ;
-	PQ.emplace(0,A);
+		if (CurCity == EndCity) {
+			std::cout << CurValue;
+			break;
+		}
 
-	while (!PQ.empty())
-	{
-		std::pair<int,int> Cur = PQ.top(); PQ.pop();
-		int CurrentCost = Cur.first;
-		int CurrentCity = Cur.second;
 
-		if (CurrentCost > Dist[CurrentCity]) continue;
-
-		if (CurrentCity == B) break;
-
-		const std::vector<std::pair<int,int>>& BusData = Costs[CurrentCity];
-		for (const std::pair<int,int>& Bus : BusData)
-		{
-			int NextCity = Bus.first;
-			int BusCost = Bus.second;
-			int NewCost = CurrentCost + BusCost;
-			if (NewCost < Dist[NextCity])
-			{
-				Dist[NextCity] = NewCost;
-				PQ.emplace(NewCost, NextCity);
+		for (const Bus& NextCityBus : BusData[CurCity]) {
+			int NewValue = CurValue + NextCityBus.Value;
+			if (NewValue < Dist[NextCityBus.End]) {
+				PQ.emplace(NewValue, NextCityBus.End);
+				Dist[NextCityBus.End] = NewValue;
 			}
 		}
 	}
-	std::cout<<Dist[B]<<"\n";
 }
-
-
-// 한 도시에서 출발
-// 다른 도시 도착하는 M개의 버스
-// A번째 도시에서 B번째 도시까지 가는데 드는 최소비용
